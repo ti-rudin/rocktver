59b7c4b01135e7fb76076e5c70ad539e7c8f4eaa
root <root@dev0.ti-robots.ru>
2022-03-08 00:09:34 +0300

    11

--- swyxkit/src/routes/index.svelte
+++ swyxkit/src/routes/index.svelte
@@ -181,7 +181,7 @@
 		border-radius: 3px;
 		box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.7);
 		margin: 0 7px;
-		padding: 18px 0;
+		padding: 12px 0;
 		display: inline-block;
 		position: relative;
 	}
@@ -189,7 +189,7 @@
 	.countdown .tiles > span:before {
 		content: '';
 		width: 100%;
-		height: 13px;
+		height: 10px;
 		background: #111;
 		display: block;
 		padding: 0 3px;
