75d207fb3764f2ce1a70587dcc677a0392653633
root <root@dev0.ti-robots.ru>
2022-03-08 04:21:22 +0300

    11

--- swyxkit/src/mdsvexlayout.svelte
+++ swyxkit/src/mdsvexlayout.svelte
@@ -1,5 +1,38 @@
 <!-- https://tailwindcss.com/docs/typography-plugin#element-modifiers -->
-<article
+<script>
+	import { onDestroy, onMount } from 'svelte';  
+import {
+    blur,
+    crossfade,
+    draw,
+    fade,
+    fly,
+    scale,
+    slide,
+  } from 'svelte/transition';
+
+  let show = false;
+  let interval;
+
+
+  onMount(() => {
+    interval = setTimeout(() => {
+      show = !show;
+    }, 1);
+  })
+
+  onDestroy(() => {
+	if (interval) {
+	  clearTimeout(interval);
+    }  
+  });
+</script>
+
+{#if show}
+
+
+
+<article transition:blur="{{duration: 400 }}" 
 	class="container prose mx-auto mb-12 hover:prose-a:text-orange-300
 prose-ul:list-disc
 dark:prose-invert
@@ -7,3 +40,4 @@ dark:prose-invert
 >
 	<slot />
 </article>
+{/if}
