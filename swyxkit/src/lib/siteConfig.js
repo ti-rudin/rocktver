c975a5a1da3b29e79af408247ef645744b466e6d
root <root@dev0.ti-robots.ru>
2022-03-09 15:55:15 +0300

    11

--- swyxkit/src/lib/siteConfig.js
+++ swyxkit/src/lib/siteConfig.js
@@ -1,11 +1,11 @@
-export const SITE_URL = 'localhost:3000';
+export const SITE_URL = 'https://rocktver.ru';
 
 export const GH_USER_REPO = 'ti-rudin/rocktver'; // used for pulling github issues and offering comments
 export const REPO_URL = 'https://github.com/' + GH_USER_REPO;
 export const SITE_TITLE = 'RockTver';
 export const SITE_DESCRIPTION = "Музыкальный праздник РОКОПОЛЧЕНИЕ-2022";
 export const DEFAULT_OG_IMAGE =
-	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
+	'https://admin.rocktver.ru/uploads/thumbnail_180_59335eda76.png';
 export const MY_TWITTER_HANDLE = 'swyx';
 export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
 
