2070a74d5e347221c9f709b4a6e2e2c7357ddb67
root <root@dev0.ti-robots.ru>
2022-03-13 23:15:22 +0300

    track now

---
+++ frontend/src/routes/3.svelte
@@ -0,0 +1,390 @@
+<script context="module">
+  export async function load({ fetch }) {
+    const res = await fetch('https://admin.rocktver.ru/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: ` {
+          artists { 
+                  data{
+                    id
+                    attributes{
+                      name
+                      avatar{
+                        data{
+                          attributes{
+                            url
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+        }`,
+      }),
+    })
+    if (res.ok) {
+      const { data } = await res.json()
+      console.log(data)
+      return {
+        props: {
+          launches: data.artists.data,
+        },
+      }
+    }
+    return {
+      status: res.status,
+      error: new Error(`Error fetching GraphQL data`),
+    }
+  }
+</script>
+
+<script>
+  export let launches
+  //console.log(this.artists.data[0].attributes.name)
+
+</script>
+
+
+<div class="h-screen w-screen flex bg-gray-200">
+	<!-- container -->
+
+	<aside
+		class="hidden lg:flex flex-col items-center bg-white text-gray-700 shadow
+		h-full">
+		<!-- Side Nav Bar-->
+
+		<div class="h-16 flex items-center w-full">
+			<!-- Logo Section -->
+			<a class="h-6 w-6 mx-auto" href="http://svelte.dev/">
+				<img
+					class="h-6 w-6 mx-auto"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/512px-Svelte_Logo.svg.png"
+					alt="svelte logo" />
+			</a>
+		</div>
+
+		<ul>
+			<!-- Items Section -->
+			<li class="hover:bg-gray-100">
+				<a
+					href="."
+					class="h-16 px-6 flex flex justify-center items-center w-full
+					focus:text-orange-500">
+					<svg
+						class="h-5 w-5"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round">
+						<polyline
+							points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
+						<path
+							d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0
+							2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0
+							0-1.79 1.11z"></path>
+					</svg>
+
+				</a>
+			</li>
+
+			<li class="hover:bg-gray-100">
+				<a
+					href="."
+					class="h-16 px-6 flex flex justify-center items-center w-full
+					focus:text-orange-500">
+					<svg
+						class="h-5 w-5"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round">
+						<path
+							d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+					</svg>
+
+				</a>
+			</li>
+
+			<li class="hover:bg-gray-100">
+				<a
+					href="."
+					class="h-16 px-6 flex flex justify-center items-center w-full
+					focus:text-orange-500">
+					<svg
+						class="h-5 w-5"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round">
+						<circle cx="12" cy="12" r="3"></circle>
+						<path
+							d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1
+							0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0
+							0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2
+							2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0
+							0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1
+							0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0
+							0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65
+							0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0
+							1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0
+							1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2
+							0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0
+							1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0
+							2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0
+							0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65
+							1.65 0 0 0-1.51 1z"></path>
+					</svg>
+				</a>
+			</li>
+
+		</ul>
+
+		<div class="mt-auto h-16 flex items-center w-full">
+			<!-- Action Section -->
+			<button
+				class="h-16 w-10 mx-auto flex flex justify-center items-center
+				w-full focus:text-orange-500 hover:bg-red-200 focus:outline-none">
+				<svg
+					class="h-5 w-5 text-red-700"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round">
+					<path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+					<polyline points="16 17 21 12 16 7"></polyline>
+					<line x1="21" y1="12" x2="9" y2="12"></line>
+				</svg>
+
+			</button>
+		</div>
+
+	</aside>
+
+	<div class="flex-1 flex flex-col">
+		<nav class="px-4 flex justify-between bg-white h-16 border-b-2">
+			<!-- top bar -->
+
+			<ul class="flex items-center lg:hidden">
+				<!-- top bar left -->
+				<li class="h-6 w-6">
+					<img
+						class="h-full w-full mx-auto"
+						src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/512px-Svelte_Logo.svg.png"
+						alt="svelte logo" />
+				</li>
+			</ul>
+
+			<ul class="flex items-center">
+				<!-- top bar center -->
+				<li>
+					<h1 class="pl-10 lg:pl-0 text-gray-700">Svelte</h1>
+				</li>
+			</ul>
+
+			<ul class="flex items-center">
+				<!-- to bar right  -->
+
+				<li class="pr-4">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="feather feather-bell">
+						<path
+							d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+						<path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+					</svg>
+
+				</li>
+
+				<li class="h-8 w-8">
+					<img
+						class="h-full w-full rounded-full mx-auto"
+						src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+						alt="profile woman" />
+				</li>
+
+			</ul>
+
+		</nav>
+
+    <ul>
+      {#each launches as launch}
+        <li>
+          <a
+            class="card-link"
+            target="_blank"
+            rel="noopener"
+            href={launch.attributes.name}
+          >
+            <h2>{launch.attributes.name}</h2>
+            <h2>{launch.id}</h2>
+    
+            <img
+              src={'https://admin.rocktver.ru' +
+                launch.attributes.avatar.data.attributes.url}
+              alt=""
+            />
+          </a>
+          
+        </li>
+      {/each}
+    </ul>
+	</div>
+
+	<nav
+		class="fixed bottom-0 w-full border bg-white lg:hidden flex
+		overflow-x-auto">
+		<!-- Bottom Icon Navigation Menu -->
+
+		<a
+			href="."
+			class="flex flex-col flex-grow items-center justify-center
+			overflow-hidden whitespace-no-wrap text-sm transition-colors
+			duration-100 ease-in-out hover:bg-gray-200 focus:text-orange-500">
+
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round">
+				<polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
+				<path
+					d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0
+					2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path>
+			</svg>
+
+			<span class="hidden text-sm capitalize">Inbox</span>
+		</a>
+
+		<a
+			href="."
+			class="flex flex-col flex-grow items-center justify-center
+			overflow-hidden whitespace-no-wrap text-sm transition-colors
+			duration-100 ease-in-out hover:bg-gray-200 text-orange-500">
+
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="feather feather-github">
+				<path
+					d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0
+					0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77
+					5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7
+					0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0
+					0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+			</svg>
+
+			<span class="text-sm capitalize">github</span>
+		</a>
+
+		<a
+			href="."
+			class="flex flex-col flex-grow items-center justify-center
+			overflow-hidden whitespace-no-wrap text-sm transition-colors
+			duration-100 ease-in-out hover:bg-gray-200 focus:text-orange-500">
+
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="feather feather-bookmark">
+				<path
+					d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+			</svg>
+
+			<span class="hidden text-sm capitalize">bookmark</span>
+		</a>
+
+		<a
+			href="."
+			class="flex flex-col flex-grow items-center justify-center
+			overflow-hidden whitespace-no-wrap text-sm transition-colors
+			duration-100 ease-in-out hover:bg-gray-200 focus:text-orange-500">
+
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round">
+				<circle cx="12" cy="12" r="3"></circle>
+				<path
+					d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83
+					2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65
+					0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0
+					0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2
+					2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0
+					0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0
+					4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2
+					0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0
+					1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1
+					1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0
+					0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0
+					1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0
+					0-1.51 1z"></path>
+			</svg>
+
+			<span class="hidden text-sm capitalize">Settings</span>
+		</a>
+
+	</nav>
+
+</div>
+
+
+
+<style>
+  
+</style>
---
+++ src/routes/2.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+	import {
+		isDarkFlag
+	} from '$lib/siteConfig';
+
+	export const prerender = true; // index page is most visited, lets prerender
+
+
+</script>
+
+<script>
+	import { onDestroy, onMount } from 'svelte';
+	import { blur, crossfade, draw, fade, fly, scale, slide } from 'svelte/transition';
+	import Carousel from '../components/Carusel.svelte';
+	import Now from '../components/Now.svelte'
+	import { ChevronLeftIcon, ChevronRightIcon } from 'svelte-feather-icons';
+
+
+
+	onMount(() => {
+		//comandes_data = launches;
+		//	console.log(comandes_data);
+	});
+</script>
+
+<Now/>
