0fb2bde530abd122b8a5831bb248a959cb6a4ca6
Alexander Rudin <ax.rudin@gmail.com>
2022-03-15 23:18:30 +0000

    11

--- src/routes/5.svelte
+++ src/routes/5.svelte
@@ -30,20 +30,169 @@
         index = await getIdTrackNow();
         if (index >= 10) { index = 0}
     }
+///////////////////////////////////////////////////
+	export let ready_comandes_data = [];
+	export let ready_track_data = [];
+    export let track_now_data = {
+						band_name: '',
+						band_id: '',
+						track_id: '',
+						track_name: '',
+						track_text: ''
+					};
+    export let track_now = 1;
+	async function load() {
+		const res = await fetch('https://admin.rocktver.ru/graphql', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				query: `{
+	            bands {
+	              data {
+	                id
+				
+	                attributes {
+                    artists{
+                      data{
+						id
+                        attributes{
+                          name
+                          avatar{
+                            data{
+                              attributes{url}
+                            }
+                          }
+                          
+                        }
+                      }
+                    }
+	                  band_name
+	                  town
+	                  small_text
+	                  group_logo{
+	                    data{
+	                      attributes{
+	                        url
+	                      }
+	                    }
+	                  }
+	                  group_link_vk
+	                  spisok {
+	                    ... on ComponentPesniTrack {
+						
+	                      id
+	                      name
+	                      text
+	                    }
+	                  }
+	                }
+	              }
+	            }
+	          }`
+			})
+		});
+
+		if (res.ok) {
+			const { data } = await res.json();
+
+			const comandes_data = data.bands.data;
+
+			ready_comandes_data = await comandes_data.map((item) => {
+				const container = {};
+				//if (item.attributes ){
+				//	container[item.path] = item.attributes.group_logo.data.attributes.url;
+				//}
+				container.band_name = item.attributes.band_name;
+				if (item.attributes.group_logo.data !== null) {
+					container.path =
+						'https://admin.rocktver.ru' + item.attributes.group_logo.data.attributes.url;
+				} else {
+					container.path = 'rock-band-icon-9.jpg';
+				}
+				container.id = 'images' + item.id;
+				container.url = 'band/' + container.band_name;
+				return container;
+			});
+
+			let ready_artists_data = [];
+			comandes_data.forEach((item) => {
+				let artists = item.attributes.artists.data;
+				let tracks = item.attributes.spisok;
+
+				artists.forEach((item) => {
+					let url = 'icon-person-10.jpg';
+					if (item.attributes.avatar.data !== null) {
+						url = 'https://admin.rocktver.ru' + item.attributes.avatar.data.attributes.url;
+					}
+
+					const container = {
+						path: url,
+						id: item.id,
+						url: 'person/' + item.attributes.name
+					};
+					ready_artists_data = [...ready_artists_data, container];
+				});
+
+				tracks.forEach((track) => {
+					const container2 = {
+						band_name: item.attributes.band_name,
+						band_id: item.id,
+						track_id: track.id,
+						track_name: track.name,
+						track_text: track.text
+					};
+
+					ready_track_data = [...ready_track_data, container2];
+				});
+			});
+
+			//console.log(ready_artists_data);
+			//console.log(data);
+			console.log(ready_track_data);
+
+			return {
+				props: {
+					ready_comandes_data: ready_comandes_data,
+					ready_artists_data: ready_artists_data,
+                    ready_track_data: ready_track_data
+				}
+			};
+		}
+		return {
+			status: res.status,
+			error: new Error(`Error fetching GraphQL data`)
+		};
+	}
+
+	import { onMount } from 'svelte';
+
+
+	onMount(() => {
+		//comandes_data = launches;
+		//	console.log(comandes_data);
+		load();
+        
+	});
+    $: track_now_data = ready_track_data[track_now];
+    
+    $: console.log(track_now_data);
+
+    export let trackname = '';
+  $: if (track_now_data) {
+        trackname = track_now_data.track_name;
+    }
+
+//////////////////////////////////////////////////////
+
 </script>
 
 <p>index: [{index}] scrollPos: {Math.trunc(scrollPos)}px</p>
-<label
-	>imagable
-	<input
-		type="checkbox"
-		bind:checked={imagable}
-		on:change={() => (width = imagable ? 'auto' : '50%')}
-	/>
-</label>
+
 
 <section style="--gap: {gap}px; --width: {width}" tab-index="0">
-	{#await getItems(page) then items}
+
 		<ul
 			use:slidy={{
 				index,
@@ -58,23 +207,23 @@
 				scroller: (p) => (scrollPos = p)
 			}}
 		>
-			{#each items as item, i}
+			{#each ready_track_data as item, i}
 				<li id={i} class:active={i === index}>
-					{#if imagable}
+					
 						<div
 							class="h-24 my-4 bg-gradient-to-r from-yellow-400 to-pink-500 flex justify-center items-center p-3 rounded-xl border-2 border-slate-100 shadow-lg transition-all transform-all hover:scale-105 cursor-pointer relative"
 							
 						>
 							<div class="text-slate-200 text-center">
-								<div>Alihossein</div>
+								<div>{item.track_name}</div>
 								<div class="font-mono text-xs">from-yellow-400 to-pink-500</div>
 							</div>
 						</div>
-					{/if}
+				
 				</li>
 			{/each}
 		</ul>
-	{/await}
+	
 </section>
 
 <label
