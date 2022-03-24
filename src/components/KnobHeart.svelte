026aecd0daa3cd697f2a0effc79cc1a891b127f7
Alexander Rudin <ax.rudin@gmail.com>
2022-03-24 19:49:31 +0000

    knobheart

--- src/components/KnobHeart.svelte
+++ src/components/KnobHeart.svelte
@@ -2,62 +2,27 @@
 import { onDestroy, onMount } from 'svelte';
 onMount(() => {
 	
-	let button = document.querySelector('.like-btn');
-	let icon = document.querySelector('.like-icon');
 
-	button.addEventListener('click', () => {
-		button.classList.toggle('active');
-		if (button.classList.contains('active')) {
-			createClones(button);
-		}
 	});
-
-	function randomNum(min, max) {
-		return Math.floor(Math.random() * (max - min + 1) + min);
-	}
-
-	function negativePositive() {
-		return Math.random() < 0.5 ? -1 : 1;
-	}
-
-	function createClones(button) {
-		let numberOfClones = randomNum(1, 10);
-
-		for (let i = 1; i <= numberOfClones; i++) {
-			let clone = icon.cloneNode(true);
-			let size = randomNum(8, 20);
-
-			button.appendChild(clone);
-			clone.setAttribute('width', size);
-			clone.setAttribute('height', size);
-			clone.classList.add('clone');
-			clone.style.transform = `translate(${negativePositive() * randomNum(15, 30)}px, ${
-				negativePositive() * randomNum(15, 30)
-			}px)`;
-			let removeNode = setTimeout(() => {
-				button.removeChild(clone);
-				clearTimeout(removeNode);
-			}, 800);
-		}
-	}
-	});
-
+  import { blur, crossfade, draw, fade, fly, scale, slide } from 'svelte/transition';	
 </script>
 
-<a href="#" class="like-btn">
-	<svg
+<button class="like-btn mx-auto rouded ">
+	<svg  
 		class="like-icon"
 		xmlns="http://www.w3.org/2000/svg"
-		width="24"
-		height="24"
+		width="54"
+		height="54"
 		viewBox="0 0 24 24"
 	>
 		<path
 			d="M12 4.4119c-2.826-5.695-11.999-4.064-11.999 3.27 0 7.27 9.903 10.938 11.999 15.311 2.096-4.373 12-8.041 12-15.311 0-7.327-9.17-8.972-12-3.27z"
 		/>
 	</svg>
-</a>
-
+</button>
+<svg transition:blur={{ duration: 300 }} xmlns="http://www.w3.org/2000/svg" class="h-16 w-16" fill="pink" viewBox="0 0 24 24" stroke="red" stroke-width="1">
+	<path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+  </svg>
 
 
 
@@ -69,16 +34,10 @@ onMount(() => {
   justify-content: center;
 }
 
-.like-btn:acive {
-  transform: scale(0.9, 0.9);
-}
 
-.like-btn.active {
-  animation: pop 0.6s 1;
-}
 
 .like-btn path {
-  fill: #767676;
+  fill: #c29898;
 }
 
 .like-btn.active path {
@@ -86,9 +45,5 @@ onMount(() => {
 }
 
 
-.clone {
-  position: absolute;
-  animation: moveOutwards 0.9s 1;
-  opacity: 0;
-}
+
 </style>
