14af5b95f74465323aea07e417916817d2eb0640
Alexander Rudin <ax.rudin@gmail.com>
2022-03-31 13:57:03 +0000

    11

--- src/routes/journal3.svelte
+++ src/routes/journal3.svelte
@@ -74,11 +74,7 @@ $: p = snarkdown(post);
 
 </script>
 
-
-
-{@html p}
 <iframe width="560" height="315" src="https://www.youtube.com/embed/FizAXHS98ck" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
 
 <style>
   	.lblock {
