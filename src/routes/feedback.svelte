dbccf0b4e3d28947b6cda4a46eaee189714b5a0c
Alexander Rudin <ax.rudin@gmail.com>
2022-04-03 13:07:54 +0000

    feedback fix0

--- src/routes/feedback.svelte
+++ src/routes/feedback.svelte
@@ -1,13 +1,15 @@
 
-<div class=" mx-auto">
+
     <iframe class="feedback " style="width=100% border: 0;" frameborder="0" src="http://reformal.ru/widget/985718"></iframe>
 
-</div>
+
 <style>
 .feedback{
 height: 500px;
 max-width: 42rem;
 width: 92vw;
+margin-left: auto;
+margin-right: auto;
 
 }
 </style>
