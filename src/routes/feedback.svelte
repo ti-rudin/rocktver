28a7b3b1f5b7ee3384a6629a049af7e2058141d2
Alexander Rudin <ax.rudin@gmail.com>
2022-04-04 20:12:22 +0000

    feedback fix

--- src/routes/feedback.svelte
+++ src/routes/feedback.svelte
@@ -1,17 +1,18 @@
 <script context="module">
     export const prerender = true;
 </script>
-
-    <iframe class="feedback " style="width=100% border: 0;" frameborder="0" src="http://reformal.ru/widget/985718"></iframe>
-
-
+<iframe class ="feedback max-w-xl w-full h-auto rounded-lg p-4" src="https://forms.yandex.ru/cloud/624b49cb13463115350ed95c/?iframe=1" frameborder="0" name="ya-form-624b49cb13463115350ed95c" ></iframe>
 <style>
 .feedback{
-height: 500px;
-max-width: 42rem;
-width: 92vw;
+
+
+
+
 margin-left: auto;
 margin-right: auto;
-
+background-color: antiquewhite;
+}
+.safe-content{
+    color:black;
 }
 </style>
