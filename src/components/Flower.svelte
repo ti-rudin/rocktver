d7bdf55b9ed20e60b713e4422d690ab67f54dca0
ti-rudin <ax.rudin@gmail.com>
2022-04-04 12:36:19 +0000

    now efir off

--- src/components/Flower.svelte
+++ src/components/Flower.svelte
@@ -1,47 +1,39 @@
-<svg class="spinner" viewBox="0 0 50 50">
-    <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
-  </svg>
+<div class="dot z-0"></div>
 
   <style lang="scss">
+$dots: 100;
 
-.spinner {
-  animation: rotate 2s linear infinite;
-  z-index: 2;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin: -25px 0 0 -25px;
-  width: 50px;
-  height: 50px;
-  
-  & .path {
-    stroke: red;
-    stroke-linecap: round;
-    animation: dash 1.5s ease-in-out infinite;
-  }
-  
+@function rando($n) {
+  $val : ();
+	@for $j from 1 through $n {
+		$val : append($val, rgb(random(250),random(250),random(250)));
+	}
+  @return $val;
+}
+$c1: rando($dots);
+$c2: rando($dots);
+@function dot($n,$arr) {
+	$val : 0 0 0 0 transparent;
+	@for $j from 1 through $n {
+		$val :  #{$val}, #{random(100)}vw #{random(100)}vh #{random(10)}vmin #{random(10)}vmin rgba(nth($arr, ($j%length($arr)) + 1), random(100)/100);
+	}
+  @return $val;
+}
+@keyframes bs{
+	100%{
+		box-shadow: dot($dots,$c2);
+	}	
+}
+.dot{
+	animation: bs 13s ease-in-out alternate infinite;
+	border-radius: 50%;
+	box-shadow: dot($dots,$c1);
+	height: 10px;
+	transform: translate(-100%,-100%);
+	transition: height 5s ease-out, width 5s ease-out;
+	width: 10px;
 }
 
-@keyframes rotate {
-  100% {
-    transform: rotate(360deg);
-  }
-}
-
-@keyframes dash {
-  0% {
-    stroke-dasharray: 1, 150;
-    stroke-dashoffset: 0;
-  }
-  50% {
-    stroke-dasharray: 90, 150;
-    stroke-dashoffset: -35;
-  }
-  100% {
-    stroke-dasharray: 90, 150;
-    stroke-dashoffset: -124;
-  }
-}
 
 
   </style>
