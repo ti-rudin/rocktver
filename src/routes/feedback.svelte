0f07a7dc420b608fb595543c448264325d6514c4
Alexander Rudin <ax.rudin@gmail.com>
2022-04-09 14:00:24 +0000

    11

--- src/routes/feedback.svelte
+++ src/routes/feedback.svelte
@@ -4,7 +4,7 @@
 <iframe class ="feedback max-w-xl w-full h-auto rounded-lg p-4" src="https://forms.yandex.ru/cloud/624b49cb13463115350ed95c/?iframe=1" frameborder="0" name="ya-form-624b49cb13463115350ed95c" ></iframe>
 <style>
 .feedback{
-height: 37rem;
+height: 35rem;
 margin-left: auto;
 margin-right: auto;
 background-color: #e9bc69;
