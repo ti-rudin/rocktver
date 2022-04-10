9d594dc57918556079fed4db48b005320d62ae64
Alexander Rudin <ax.rudin@gmail.com>
2022-04-10 06:46:17 +0000

    11

--- src/routes/feedback.svelte
+++ src/routes/feedback.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-    export const prerender = true;
+   // export const prerender = true;
 </script>
 <iframe class ="feedback max-w-xl w-full h-auto rounded-lg p-4" src="https://forms.yandex.ru/cloud/624b49cb13463115350ed95c/?iframe=1" frameborder="0" name="ya-form-624b49cb13463115350ed95c" ></iframe>
 <style>
