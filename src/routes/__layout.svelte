3f3075bd3ec93eda5ee889166a23684fea9de11e
Alexander Rudin <ax.rudin@gmail.com>
2022-05-23 22:24:55 +0000

    116

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,11 @@
 <script>
-	import { SITE_TITLE } from '$lib/siteConfig';
+	import {
+		SITE_URL,
+		SITE_TITLE,
+		SITE_DESCRIPTION,
+		DEFAULT_OG_IMAGE,
+		isDarkFlag
+	} from '$lib/siteConfig';
 
 	import '../tailwind.css';
 	import Nav from '../components/Nav.svelte';
@@ -14,7 +20,23 @@
 	});
 </script>
 
-<svelte:head />
+
+
+<svelte:head>
+	<title>{SITE_TITLE}</title>
+	<link rel="canonical" href={SITE_URL} />
+	<meta property="og:url" content={SITE_URL} />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={SITE_TITLE} />
+	<meta name="Description" content={SITE_DESCRIPTION} />
+	<meta property="og:description" content={SITE_DESCRIPTION} />
+	<meta property="og:image" content={DEFAULT_OG_IMAGE} />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:creator" content={'@' + SITE_TITLE} />
+	<meta name="twitter:title" content={SITE_TITLE} />
+	<meta name="twitter:description" content={SITE_DESCRIPTION} />
+	<meta name="twitter:image" content={DEFAULT_OG_IMAGE} />
+</svelte:head>
 
 <div class="flex flex-col justify-center bg-white/70 px-4 dark:bg-gray-400/10 ">
 	<Nav />
--- src/routes/rockopolchenie22.svelte
+++ src/routes/rockopolchenie22.svelte
@@ -81,21 +81,6 @@
 	}
 </script>
 
-<svelte:head>
-	<title>{SITE_TITLE}</title>
-	<link rel="canonical" href={SITE_URL} />
-	<meta property="og:url" content={SITE_URL} />
-	<meta property="og:type" content="article" />
-	<meta property="og:title" content={SITE_TITLE} />
-	<meta name="Description" content={SITE_DESCRIPTION} />
-	<meta property="og:description" content={SITE_DESCRIPTION} />
-	<meta property="og:image" content={DEFAULT_OG_IMAGE} />
-	<meta name="twitter:card" content="summary" />
-	<meta name="twitter:creator" content={'@' + SITE_TITLE} />
-	<meta name="twitter:title" content={SITE_TITLE} />
-	<meta name="twitter:description" content={SITE_DESCRIPTION} />
-	<meta name="twitter:image" content={DEFAULT_OG_IMAGE} />
-</svelte:head>
 
 <LogoComponent/>
 <h2 class="mx-auto mt-1  text-center ">
