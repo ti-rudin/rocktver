f3c46e4ca7d7bf324edbfd777207e7e426c6d6a7
Alexander Rudin <ax.rudin@gmail.com>
2022-05-23 23:53:34 +0000

    11

--- src/routes/lotereya/[id].svelte
+++ src/routes/lotereya/[id].svelte
@@ -72,7 +72,52 @@
 			//fetch('')
 		});
 	}
+	import { browser } from '$app/env';
 
+	function getit(response) {
+		if (response.session) {
+			var id = response.session.mid;
+		}
+		VK.Api.call(
+			'users.get',
+			{
+				uids: id,
+				fields: 'photo_max, first_name,last_name, sex, bdate, city, country, followers_count',
+				v: '5.131'
+			},
+			function (r) {
+				if (r.response) {
+					//alert(r.response.sex);
+					$isAuthenticated = true;
+				
+					localStorage.setItem('isAuthenticated', JSON.stringify(true));
+
+					//console.log(r.response);
+					let user_data = {
+						id: r.response[0]['id'],
+						bdate: r.response[0]['bdate'] ? r.response[0]['bdate'] : 'не указано',
+						name: r.response[0]['first_name'] + ' ' + r.response[0]['last_name'],
+						photo: r.response[0].photo_max,
+						city: r.response[0]['city'] ? r.response[0]['city'].title : 'не указано',
+						country: r.response[0]['country'] ? r.response[0]['country'].title : 'не указано',
+						followers_count: r.response[0].followers_count,
+						sex: r.response[0].sex ? r.response[0].sex : 'не указано'
+					};
+					user.set(user_data);
+
+			
+					localStorage.setItem('user', JSON.stringify(user_data));
+
+					ym(88086612, 'reachGoal', 'vk-auth');
+					//LogRocket.identify(r.response[0]['id'], {
+					//	name: r.response[0]['first_name'] + ' ' + r.response[0]['last_name'],
+					//	vk_id: r.response[0]['id'],
+					//	city: r.response[0]['city'] ? r.response[0]['city'].title : 'не указано'
+					//});
+				}
+			}
+		);
+	}
 	onMount(() => {
 		postprepare();
 		//console.log(imgurl);
@@ -117,7 +162,47 @@
 	<meta name="twitter:image" content={imgurl} />
 </svelte:head>
 <LogoComponent />
-{#if isAuthenticated}
+<div class="mt-1 w-full ">
+	<div
+		aria-label="card 1"
+		class="mx-auto max-w-2xl cursor-pointer rounded-lg bg-blue-400/70 p-6 shadow ring-yellow-400 transition-all hover:ring-2 focus:outline-none dark:bg-blue-500 "
+	>
+	
+		<h1 class="text-2xl mx-auto">{launch.attributes.title}</h1>
+	</div>
+</div>
+
+<div
+class="loginbut cursor-pointer p-3 mx-auto mb-8 flex w-full max-w-2xl flex-col items-start rounded-lg bg-yellow-400/50 px-3 text-black ring-yellow-400 transition-all hover:ring-2 dark:bg-yellow-800/25 dark:text-white"
+id="login_button"
+on:click={() => {
+	ym(88086612, 'reachGoal', 'vk-auth-start');
+	VK.Auth.login(getit);
+}}
+>
+<p class="mx-auto">Войти с помощью</p>
+<svg
+	class="mx-auto mt-5 opacity-100"
+	width="48"
+	height="48"
+	viewBox="0 0 48 48"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<path
+		d="M0 23.04C0 12.1788 0 6.74826 3.37413 3.37413C6.74826 0 12.1788 0 23.04 0H24.96C35.8212 0 41.2517 0 44.6259 3.37413C48 6.74826 48 12.1788 48 23.04V24.96C48 35.8212 48 41.2517 44.6259 44.6259C41.2517 48 35.8212 48 24.96 48H23.04C12.1788 48 6.74826 48 3.37413 44.6259C0 41.2517 0 35.8212 0 24.96V23.04Z"
+		fill="#0077FF"
+	/>
+	<path
+		d="M25.54 34.5801C14.6 34.5801 8.3601 27.0801 8.1001 14.6001H13.5801C13.7601 23.7601 17.8 27.6401 21 28.4401V14.6001H26.1602V22.5001C29.3202 22.1601 32.6398 18.5601 33.7598 14.6001H38.9199C38.0599 19.4801 34.4599 23.0801 31.8999 24.5601C34.4599 25.7601 38.5601 28.9001 40.1201 34.5801H34.4399C33.2199 30.7801 30.1802 27.8401 26.1602 27.4401V34.5801H25.54Z"
+		fill="white"
+	/>
+</svg>
+<p class="mx-auto">Вконтакте</p>
+</div>
+
+
+{#if $isAuthenticated}
 	{#if imgurl}
 		<div class="mx-auto max-w-xl">
 			<img class="" src={imgurl} alt="" />
@@ -194,6 +279,11 @@
 
 <style>
 	.readypic {
+	}
+	.loginbut {
+		width: 200px;
+		font-size: 20px;
+		text-align: center;
 	}
 	.maskpic {
 		max-width: fit-content;
