32109455154df132e5557f7216f73729ec3b466f
Alexander Rudin <ax.rudin@gmail.com>
2022-05-24 00:15:04 +0000

    loter step 2

--- src/routes/lotereya/[id].svelte
+++ src/routes/lotereya/[id].svelte
@@ -89,7 +89,7 @@
 				if (r.response) {
 					//alert(r.response.sex);
 					$isAuthenticated = true;
-				
+
 					localStorage.setItem('isAuthenticated', JSON.stringify(true));
 
 					//console.log(r.response);
@@ -105,7 +105,6 @@
 					};
 					user.set(user_data);
 
-			
 					localStorage.setItem('user', JSON.stringify(user_data));
 
 					ym(88086612, 'reachGoal', 'vk-auth');
@@ -122,11 +121,11 @@
 		postprepare();
 		//console.log(imgurl);
 		document.getElementById('vk_share_button').innerHTML = VK.Share.button(
-			{ 
+			{
 				image: 'https://rocktver.ru/2930.jpeg',
-				title: usernametitle,  
+				title: usernametitle,
 				noparse: true,
-				url: 'https://rocktver.ru' 
+				url: 'https://rocktver.ru'
 			},
 			{
 				type: 'round_nocount',
@@ -167,80 +166,25 @@
 		aria-label="card 1"
 		class="mx-auto max-w-2xl cursor-pointer rounded-lg bg-blue-400/70 p-6 shadow ring-yellow-400 transition-all hover:ring-2 focus:outline-none dark:bg-blue-500 "
 	>
-	
-		<h1 class="text-2xl mx-auto">{launch.attributes.title}</h1>
+		<h1 class="mx-auto text-2xl">{launch.attributes.title}</h1>
 	</div>
 </div>
 
-<div
-class="loginbut cursor-pointer p-3 mx-auto mb-8 flex w-full max-w-2xl flex-col items-start rounded-lg bg-yellow-400/50 px-3 text-black ring-yellow-400 transition-all hover:ring-2 dark:bg-yellow-800/25 dark:text-white"
-id="login_button"
-on:click={() => {
-	ym(88086612, 'reachGoal', 'vk-auth-start');
-	VK.Auth.login(getit);
-}}
->
-<p class="mx-auto">Войти с помощью</p>
-<svg
-	class="mx-auto mt-5 opacity-100"
-	width="48"
-	height="48"
-	viewBox="0 0 48 48"
-	fill="none"
-	xmlns="http://www.w3.org/2000/svg"
->
-	<path
-		d="M0 23.04C0 12.1788 0 6.74826 3.37413 3.37413C6.74826 0 12.1788 0 23.04 0H24.96C35.8212 0 41.2517 0 44.6259 3.37413C48 6.74826 48 12.1788 48 23.04V24.96C48 35.8212 48 41.2517 44.6259 44.6259C41.2517 48 35.8212 48 24.96 48H23.04C12.1788 48 6.74826 48 3.37413 44.6259C0 41.2517 0 35.8212 0 24.96V23.04Z"
-		fill="#0077FF"
-	/>
-	<path
-		d="M25.54 34.5801C14.6 34.5801 8.3601 27.0801 8.1001 14.6001H13.5801C13.7601 23.7601 17.8 27.6401 21 28.4401V14.6001H26.1602V22.5001C29.3202 22.1601 32.6398 18.5601 33.7598 14.6001H38.9199C38.0599 19.4801 34.4599 23.0801 31.8999 24.5601C34.4599 25.7601 38.5601 28.9001 40.1201 34.5801H34.4399C33.2199 30.7801 30.1802 27.8401 26.1602 27.4401V34.5801H25.54Z"
-		fill="white"
-	/>
-</svg>
-<p class="mx-auto">Вконтакте</p>
-</div>
-
-
 {#if $isAuthenticated}
-	{#if imgurl}
-		<div class="mx-auto max-w-xl">
-			<img class="" src={imgurl} alt="" />
-		</div>
-	{:else}
-		<div class="mx-auto max-w-xl">
-			<div
-				bind:this={htmlimg}
-				class="maskpic mx-auto  flex flex-col bg-white p-4 text-black dark:bg-gray-900 dark:text-white"
-			>
-				<div aria-label="card 1" class="mx-auto rounded-lg">
-					<div class="readypic">
-						<Ramka1 img={imguserurl} />
-					</div>
-					<h2 class="name mr-auto ml-3">{$user.name}</h2>
-				</div>
-			</div>
-		</div>
-	{/if}
-	<div class="mt-3 w-full">
-		<div
-			aria-label="card 1"
-			class="mx-auto max-w-2xl rounded-lg bg-blue-400/50 pb-3 shadow dark:bg-blue-500/20 "
-		>
-			<div class=" mx-auto p-4 text-center text-gray-800 focus:outline-none dark:text-white">
-				Скачайте готовую картинку для постинга на любых своих ресурсах.<br />
-				Помогите узнать о нас как можно больше широкому кругу людей!
-			</div>
+	<p class="slogan mx-auto mb-5">Спасибо, что Вы с нами!</p>
+	<div class="flex justify-center">
+		<!-- Card -->
+		<div class="delay-50 group flex w-auto rounded-lg bg-gray-800 p-5">
+			<!-- Image Cover -->
+			<img class="h-28 w-28 w-full rounded shadow" src={$user.photo} alt={$user.name} />
+			<div class="mx-10 flex flex-col">
+				<!-- Title -->
+				<h3 class="font-bold text-gray-200">{$user.name}</h3>
 
-			<div
-				aria-label="card 1"
-				class="mx-auto mb-2 max-w-[60%] cursor-pointer rounded-lg bg-blue-400/70 p-2 text-center text-xl shadow ring-1 ring-yellow-400 transition-all hover:ring-2 focus:outline-none dark:bg-blue-300/40 "
-				on:click={() => {
-					download();
-					ym(88086612, 'reachGoal', 'download pic');
-				}}
-			>
-				Скачать открытку
+				<!-- Description -->
+				<p class="mt-2 text-sm font-bold text-gray-400">Страна: {$user.country}</p>
+				<p class="mt-2 text-sm font-bold text-gray-400">Город: {$user.city}</p>
+				<p class="mt-2 text-sm font-bold text-gray-400">ВК ID: {$user.id}</p>
 			</div>
 		</div>
 	</div>
@@ -251,7 +195,7 @@ on:click={() => {
 			class="mx-auto max-w-2xl rounded-lg bg-blue-400/50 pb-3 shadow dark:bg-blue-500/20 "
 		>
 			<div class=" mx-auto p-4 text-center text-gray-800 focus:outline-none dark:text-white">
-				Поделитесь этой страницей в социальных сетях!<br />
+				Для участия в лотерее осталось совсем немного. Поделитесь информацией о нашем празднике в ВК!<br />
 				Помогите узнать о нас как можно больше широкому кругу людей!
 			</div>
 
@@ -265,6 +209,35 @@ on:click={() => {
 		</div>
 	</div>
 {:else}
+	<div
+		class="loginbut cursor-pointer p-3 mx-auto mb-8 flex w-full max-w-2xl flex-col items-start rounded-lg bg-yellow-400/50 px-3 text-black ring-yellow-400 transition-all hover:ring-2 dark:bg-yellow-800/25 dark:text-white"
+		id="login_button"
+		on:click={() => {
+			ym(88086612, 'reachGoal', 'vk-auth-start');
+			VK.Auth.login(getit);
+		}}
+	>
+		<p class="mx-auto">Войти с помощью</p>
+		<svg
+			class="mx-auto mt-5 opacity-100"
+			width="48"
+			height="48"
+			viewBox="0 0 48 48"
+			fill="none"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M0 23.04C0 12.1788 0 6.74826 3.37413 3.37413C6.74826 0 12.1788 0 23.04 0H24.96C35.8212 0 41.2517 0 44.6259 3.37413C48 6.74826 48 12.1788 48 23.04V24.96C48 35.8212 48 41.2517 44.6259 44.6259C41.2517 48 35.8212 48 24.96 48H23.04C12.1788 48 6.74826 48 3.37413 44.6259C0 41.2517 0 35.8212 0 24.96V23.04Z"
+				fill="#0077FF"
+			/>
+			<path
+				d="M25.54 34.5801C14.6 34.5801 8.3601 27.0801 8.1001 14.6001H13.5801C13.7601 23.7601 17.8 27.6401 21 28.4401V14.6001H26.1602V22.5001C29.3202 22.1601 32.6398 18.5601 33.7598 14.6001H38.9199C38.0599 19.4801 34.4599 23.0801 31.8999 24.5601C34.4599 25.7601 38.5601 28.9001 40.1201 34.5801H34.4399C33.2199 30.7801 30.1802 27.8401 26.1602 27.4401V34.5801H25.54Z"
+				fill="white"
+			/>
+		</svg>
+		<p class="mx-auto">Вконтакте</p>
+	</div>
+
 	<div class="mt-1 w-full ">
 		<div
 			aria-label="card 1"
