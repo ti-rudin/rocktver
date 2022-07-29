03f808163514ccb24945f663b03c585f7e07cc7b
Alexander Rudin <ax.rudin@gmail.com>
2022-07-29 12:16:07 +0000

    11

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		window.location.href = '/now'
-		//window.location.href = '/rockopolchenie22/';
+		//window.location.href = '/now'
+		window.location.href = '/rockopolchenie22/';
 	});
 </script>
